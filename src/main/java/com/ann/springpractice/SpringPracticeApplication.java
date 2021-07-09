baa4a9f7875c2436522ff76d0ddb17b6412302c9
Ann Michelle Gonzales <ann.michelle.gonzales112@gmail.com>
2021-07-08 20:14:12 -0500

    enable auto config

--- src/main/java/com/ann/springpractice/SpringPracticeApplication.java
+++ src/main/java/com/ann/springpractice/SpringPracticeApplication.java
@@ -1,8 +1,11 @@
 package com.ann.springpractice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @SpringBootApplication
 public class SpringPracticeApplication {
 
